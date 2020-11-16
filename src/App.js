4dde448fc34245226f0a3b19973f32163e86f1a3
siddiq <siddiq.jalalu@gmail.com>
2020-11-16 21:33:06 +0530

    prettier space added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import { Route, Switch, Link } from 'react-router-dom';
+//import PropTypes from 'prop-types';
 import './App.css';
 import { RoutePages } from './Routes/Index';
 
 function App() {
-return (
-  <div className="App">
-    <Switch>
-      <Route
-        path="/" render={() => {
-          return (
-            <RoutePages />
-          );
-        }}
+  return (
+    <div className="App">
+      <Switch>
+        <Route
+          path="/"
+          render={() => {
+            return <RoutePages />;
+          }}
         />
-    </Switch>
-      <Link
-        to="/program"
-      >
-        Student
-        </Link>
-   </div>
+      </Switch>
+      <Link to="/program">Student</Link>
+    </div>
   );
 }
 
--- src/Modules/Student/index.js
+++ src/Modules/Student/index.js
@@ -1,33 +1,35 @@
-import React, { Component } from "react";
-import { Route, Switch } from "react-router";
-import StudentList from "./components/student_list"
-
-import {
-    withRouter
-} from "react-router-dom";
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router';
+import { withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import StudentList from './components/student_list';
 
 class Student extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    render() {
-        return (
-
-            <div className="">
-                <button onClick={() => this.props.history.push({ pathname: "/program/listv1" })}>list</button>
-                <Switch>
-                    <Route exact path="/program/listv1" component={StudentList}>
-                    </Route>
-                </Switch>
-            </div>
-        );
-    }
-
+  render() {
+    const { history } = this.props;
+    return (
+      <div className="">
+        <button
+          type="button"
+          onClick={() => history.push({ pathname: '/program/listv1' })}
+        >
+          list
+        </button>
+        <Switch>
+          <Route exact path="/program/listv1" component={StudentList} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
+Student.propTypes = {
+  history: PropTypes.object,
+};
 
 export default withRouter(Student);
-
